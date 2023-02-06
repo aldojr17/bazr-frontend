929d7890ada50eee456926e3c54eec39a561a57c
Hidayat Hidayat <hidayat.hidayat@shopee.com>
2023-02-06 11:28:54 +0700

    fix: handle page not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,8 +1,31 @@
+import { Button, Flex, Heading, Text, VStack } from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
+import routes from "../../routes/Routes";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
+    <Flex
+      justifyContent="center"
+      alignItems="center"
+      w="100vw"
+      h="100vh"
+      zIndex={9999}
+      bg="gray.100"
+    >
+      <VStack alignItems={"start"} gap={6}>
+        <VStack alignItems={"start"}>
+          <Heading>Error 404: Page Not Found</Heading>
+          <Text fontWeight={"semibold"} color={"darkLighten"}>
+            We are working on something new... Stay tuned!
+          </Text>
+        </VStack>
+        <Button colorScheme="linkedin" onClick={() => navigate(routes.HOME)}>
+          Back To Home
+        </Button>
+      </VStack>
+    </Flex>
   );
 };
 
